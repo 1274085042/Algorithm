8ea7f05d85e1b4bd2794518c479e058852b2dadf
秦时明月 <a1274085042@outlook.com>
2019-12-14 00:14:55 +0800

    字符串排序

---
+++ Offer/Str_Permutaion/str_pailie.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void Permutation(string );
+void Permutation(string str, int begin);  //str代表整个字符串，begin：待排列字符串中第一个字符的下标
+
+int main()
+{
+	//char *s = "abc";
+	string s = "abc";
+	//cout<<s<<endl;
+	Permutation(s);
+	system("pause");
+	return 0;
+}
+
+void Permutation(string str)
+{
+	if (str.size() == 0)
+		return;
+	else
+		Permutation(str, 0);
+}
+
+void Permutation(string str, int  begin)
+{
+	if (str[begin] == '\0')
+	{
+		cout << str << endl;
+	}
+	for (int c = begin; str[c] != '\0'; c++)   //c：待交换字符的下标
+	{
+		char temp = str[c];
+		str[c] = str[begin];
+		str[begin] = temp;
+		Permutation(str, begin + 1);
+
+		temp = str[c];
+		str[c] = str[begin];
+		str[begin] = temp;
+	}
+
+}
